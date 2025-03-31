3745bbc78fc50a4838930fc47d1a35f38d4cceb2
IWonderWhatThisAPIDoes <tomas.cajan@seznam.cz>
2025-03-31 21:41:19 +0200

    demo: Add credits to binaries where due

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import banner from 'vite-plugin-banner';
+import bannerJs from './assets/banner-js.txt';
+import bannerCss from './assets/banner-css.txt';
 
 export default defineConfig({
     build: {
-        minify: false,
         outDir: 'out',
     },
-    plugins: [wasm(), topLevelAwait()],
+    plugins: [
+        wasm(),
+        topLevelAwait(),
+        banner(fileName => {
+            if (fileName.endsWith('.js')) {
+                return bannerJs;
+            } else if (fileName.endsWith('.css')) {
+                return bannerCss;
+            }
+        }),
+    ],
 });
