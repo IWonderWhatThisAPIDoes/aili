dfa6f9eda33bd0401a9e44c315071d6eda9270c9
IWonderWhatThisAPIDoes <tomas.cajan@seznam.cz>
2025-04-01 20:23:07 +0200

    demo: Use better whitespace handling in stylesheet

--- demo/src/stylesheet.ts
+++ demo/src/stylesheet.ts
@@ -15,17 +15,12 @@ export class StylesheetInput {
         this.resetStylesheet();
     }
     get stylesheetText(): string {
-        // Drop non-breaking spaces from input
-        return this.panel.innerText.replace(/\xa0/g, ' ');
-    }
-    set stylesheetText(stylesheet: string) {
-        // Make spaces non-breaking so they do not get omited
-        this.panel.innerText = stylesheet.replace(/ /g, '\xa0')
+        return this.panel.innerText;
     }
     resetStylesheet(stylesheetId?: BuiltinStylesheet | undefined): void {
         stylesheetId ??= this.currentBaseStylesheet;
         this.currentBaseStylesheet = stylesheetId;
-        this.stylesheetText = getSampleStylesheet(stylesheetId);
+        this.panel.innerText = getSampleStylesheet(stylesheetId);
     }
     private panel: HTMLElement;
     private currentBaseStylesheet: BuiltinStylesheet = BuiltinStylesheet.DEFAULT;
