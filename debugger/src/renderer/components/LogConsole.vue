34a03494bd17db26864f84777471008460975d66
IWonderWhatThisAPIDoes <tomas.cajan@seznam.cz>
2025-04-30 10:35:04 +0200

    debugger: Limit length of log history

--- debugger/src/renderer/components/LogConsole.vue
+++ debugger/src/renderer/components/LogConsole.vue
@@ -8,7 +8,15 @@
     /**
      * Data of a single line of log.
      */
-     export type LogEntry = readonly [readonly string[], Severity, string, string | undefined];
+    export type LogEntry = readonly [readonly string[], Severity, string, string | undefined];
+    /**
+     * How many lines of history should be kept by default.
+     * 
+     * History is deleted in buffers of this size,
+     * so there will always be between {@link DEFAULT_HISTORY_BUFFER_SIZE}
+     * and {@link DEFAULT_HISTORY_BUFFER_SIZE} `* 2` lines.
+     */
+    const DEFAULT_HISTORY_BUFFER_SIZE: number = 1000;
 </script>
 
 <script setup lang="ts">
@@ -17,9 +25,11 @@
     import Console from './Console.vue';
     import LogLine from './LogLine.vue';
 
-    const { showTopic } = defineProps<{ showTopic?: boolean }>();
+    const { showTopic, history } = defineProps<{ showTopic?: boolean, history?: number }>();
 
-    const logs = ref<LogEntry[]>([]);
+    // Two swappable buffers
+    // Once one of them is full, it is pushed back and the previous one is discarded
+    const logs = ref<[LogEntry[], LogEntry[]]>([[], []]);
 
     const scrollToBottomOfContainingScrollBox = inject(INJECT_SCROLL_TO_BOTTOM, undefined);
 
@@ -30,7 +40,12 @@
          * @param entry Data that describes the new line.
          */
         addEntry(...entry: LogEntry) {
-            logs.value.push(entry);
+            logs.value[1].push(entry);
+            // Swap buffers if this one is full
+            if (logs.value[1].length >= (history ?? DEFAULT_HISTORY_BUFFER_SIZE)) {
+                logs.value[0] = logs.value[1];
+                logs.value[1] = [];
+            }
             // QOL feature: scroll to the bottom of your log console
             // when new data arrives
             nextTick(() => scrollToBottomOfContainingScrollBox?.());
@@ -39,17 +54,19 @@
          * Clears the log console.
          */
         clear() {
-            logs.value = [];
+            logs.value = [[], []];
         },
     });
 </script>
 
 <template>
     <Console>
-        <LogLine v-for="log of logs"
-            :topic="log[0]"
-            :severity="log[1]"
-            :message="log[2]"
-            :showTopic="showTopic"/>
+        <template v-for="buffer of logs">
+            <LogLine v-for="log of buffer"
+                :topic="log[0]"
+                :severity="log[1]"
+                :message="log[2]"
+                :showTopic="showTopic"/>
+        </template>
     </Console>
 </template>
