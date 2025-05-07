f82796c55560e99ecf6fb9932f55ce1d222e34ad
IWonderWhatThisAPIDoes <tomas.cajan@seznam.cz>
2025-05-07 22:45:50 +0200

    debugger: Add line numbers to style editors

--- debugger/src/renderer/components/EditorConsole.vue
+++ debugger/src/renderer/components/EditorConsole.vue
@@ -2,21 +2,41 @@
  * Editor window with terminal aesthetics.
 -->
 
+<script lang="ts">
+    /**
+     * Calculates the number of lines in a string.
+     * 
+     * @param s The source string to examine.
+     */
+    function countLines(s: string): number {
+        let length = (s.match(/\n/g)?.length ?? 0) + 1;
+        if (s.endsWith('\n')) {
+            length -= 1;
+        }
+        return length;
+    }
+</script>
+
 <script setup lang="ts">
-    import { useTemplateRef } from 'vue';
+    import { ref, useTemplateRef } from 'vue';
     import Console from './Console.vue';
 
-    defineProps<{ content?: string }>();
+    const { content } = defineProps<{ content?: string }>();
     const emit = defineEmits<{ input: [content: string] }>();
+    const lineCount = ref(content ? countLines(content) : 1);
     const inner = useTemplateRef('inner');
 
     function contentChanged(): void {
         emit('input', inner.value?.innerText ?? '');
+        lineCount.value = inner.value ? countLines(inner.value.innerText) : 1;
     }
 </script>
 
 <template>
     <Console class="editor-console">
+        <div class="editor-line-numbers">
+            <div v-for="i in lineCount">{{ i }}</div>
+        </div>
         <div class="editor-console-inner"
             contenteditable="plaintext-only"
             @input="contentChanged"
@@ -29,6 +49,14 @@
 <style>
     .editor-console {
         display: flex;
+        gap: 1em;
+    }
+
+    .editor-line-numbers {
+        color: grey;
+        text-align: right;
+        pointer-events: none;
+        user-select: none;
     }
 
     .editor-console-inner {
