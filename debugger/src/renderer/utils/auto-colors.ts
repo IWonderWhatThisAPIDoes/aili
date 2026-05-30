9140098a685fed14b97bb88e735eaa897c4de84f
IWonderWhatThisAPIDoes <tomas.cajan@seznam.cz>
2026-05-30 08:32:41 +0200

    debugger: Change auto color algorithm

--- debugger/src/renderer/utils/auto-colors.ts
+++ debugger/src/renderer/utils/auto-colors.ts
@@ -1,5 +1,5 @@
 /**
- * Deterministic color-coding of named entities.
+ * Utilities for automatic color-coding of concepts
  *
  * @module
  */
@@ -14,26 +14,33 @@ export function colorFromString(s: string): string {
     // Hash the string to ensure enough variety in generated colors
     // This is going to crash on strings that are shorter than 4 characters,
     // let us assume we do not need that many of those
-    const hash = [].reduce.call(
-        s,
-        (h: number, c: string) => {
-            // Most characters we are likely to encounter here are at least 7 bits long
-            // Some may be 6
-            // Shorter characters are not a concern here
-            const STRIDE = 6;
-            return (h << STRIDE) ^ (h + (c.codePointAt(0) ?? 0));
-        },
-        0,
-    );
-    // Not all colors are suitable
-    // Start with red or green set to maximum to ensure the colors are bright
-    // Not blue, max-blue colors are still pretty dark
-    let r = hash & 0 ? 0xff : 0x40;
-    let g = hash & 0 ? 0x40 : 0xff;
-    let b = 0;
-    // Construct the color from the hash
-    r |= (hash >> 18) & 0xff;
-    g |= (hash >> 10) & 0xff;
-    b |= (hash >> 2) & 0xff;
-    return `rgb(${r}, ${g}, ${b})`;
+    const hash = Array.from(s).reduce((h: number, c: string) => {
+        // Most characters we are likely to encounter here are at least 7 bits long
+        // Some may be 6
+        // Shorter characters are not a concern here
+        const STRIDE = 6;
+        return (h << STRIDE) ^ (h + (c.codePointAt(0) ?? 0));
+    }, 0);
+    return colorFromNumber(hash);
+}
+
+/**
+ * Constructs a randomized color.
+ *
+ * @returns CSS color specification.
+ */
+export function generateRandomColor(): string {
+    return colorFromNumber(Math.round(Math.random() * 0x10000001));
+}
+
+/**
+ * Constructs a deterministic color from a number.
+ *
+ * @param n Number that is to be color-coded.
+ * @returns CSS color specification corresponding to `n`.
+ */
+function colorFromNumber(n: number): string {
+    const h = n % 360;
+    const c = ((n / 7) % 100) + 100;
+    return `lch(70 ${c} ${h}deg)`;
 }
